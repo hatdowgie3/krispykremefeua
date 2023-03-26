efab9046a2c6204d0eff14155c0d351b4bc546e4
hatdowgie3 <128987130+hatdowgie3@users.noreply.github.com>
2023-03-26 23:07:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("nav a.anchor").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
